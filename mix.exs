d4619d8c477c213b588e10b69128e4b8485dc157
meh <meh@schizofreni.co>
2016-01-08 22:00:12 +0100

    mix: bump version and requirements

--- mix.exs
+++ mix.exs
@@ -3,14 +3,14 @@ defmodule Exquisite.Mixfile do
 
   def project do
     [ app: :exquisite,
-      version: "0.1.5",
-      elixir: "~> 1.0.0-rc1 or ~> 1.1.0-rc.0",
+      version: "0.1.6",
+      elixir: "~> 1.0.0-rc1 or ~> 1.1.0-rc.0 or ~> 1.2.0-rc.0",
       package: package,
       description: "DSL to match_spec" ]
   end
 
   defp package do
-    [ contributors: ["meh"],
+    [ maintainers: ["meh"],
       licenses: ["WTFPL"],
       links: %{"GitHub" => "https://github.com/meh/exquisite"} ]
   end
